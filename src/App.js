eb4a04a9c838ea45e7a31930e8b61adf3ea65b2d
Leonard Oduneye <chasisdude@gmail.com>
2022-07-01 08:14:22 +0100

    Finished the admin homepage

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import Topbar from "./components/topbar/Topbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import './App.css'
+import Home from "./pages/home/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import UserList from "./pages/userList/UserList";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Topbar />
+        <div className="container">
+          <Sidebar />
+          <Switch>
+            <Route exact path='/' >
+              <Home />
+            </Route>
+            <Route path='/users'>
+              <UserList />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
